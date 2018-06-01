9ca87e048982b12bc98139daf6ebafb55c11c24d
Gergely Kovacs <gergelyk89@gmail.com>
2018-06-01 11:42:50 +0200

    refactor Handler with MyHandler

--- src/com/codecool/webroute/Handler.java
+++ src/com/codecool/webroute/Handler.java
@@ -4,10 +4,13 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 
-
-public class Handler extends MyHandler implements HttpHandler{
+public class Handler implements HttpHandler{
 
     @WebRoute(route = "/")
     public void handleIndex(HttpExchange httpExchange) throws IOException{
@@ -30,6 +33,37 @@ public class Handler extends MyHandler implements HttpHandler{
         handleResponse(httpExchange, "Variable passed in route: " + routeArray[routeArray.length-1]);
     }
 
+    @Override
+    public void handle(HttpExchange httpExchange) {
+        String route = httpExchange.getRequestURI().getPath();
+        String methodType = httpExchange.getRequestMethod();
+        Method[] methods = this.getClass().getMethods();
+        for (Method method: methods) {
+            if (IsCorrectRoute(route, method.getAnnotation(WebRoute.class).route()) && method.getAnnotation(WebRoute.class).method().toString().equals(methodType)) {
+                try {
+                    method.invoke(this, httpExchange);
+                    break;
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private boolean IsCorrectRoute(String route, String webRoute) {
+        if (webRoute.contains("<")) {
+            List<String> routeArray = Arrays.asList(route.split("/"));
+            List<String> webRouteArray = Arrays.asList(webRoute.split("/"));
+            if (routeArray.size() == webRouteArray.size()) {
+                for (int i = 0; i < routeArray.size() - 1; i++) {
+                    return routeArray.get(i).equals(webRouteArray.get(i)) || webRouteArray.contains("<");
+                }
+            }
+            return false;
+        } else {
+            return route.equals(webRoute);
+        }
+    }
 
     private void handleResponse(HttpExchange httpExchange, String s) throws IOException {
         httpExchange.sendResponseHeaders(200, s.getBytes().length);
@@ -37,6 +71,4 @@ public class Handler extends MyHandler implements HttpHandler{
         os.write(s.getBytes());
         os.close();
     }
-
-
 }
